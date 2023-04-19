25d1ea28be22446f9028c6bb2c450bd32b8b4d30
jessivilla1975 <yessica.villa@correounivalle.edu.co>
2023-04-18 23:25:32 -0500

    Creacion del menu principal

--- src/main/App.java
+++ src/main/App.java
@@ -1,11 +1,14 @@
 package main;
 
+import java.util.Scanner;
+
 import logica.Gato;
 import logica.Perro;
 import logica.RazaPerro;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
 
         // Prueba con objetos Perro
         System.out.println("\nPruebas obj Perro");
@@ -44,6 +47,37 @@ public class App {
         System.out.println(gato1.apodo());
         System.out.println(garf.tributo());
         System.out.println(pancho.saludar()); 
+
+        //Menu principal
+        int opc=0;
+        do {          
+       
+            System.out.println("\n---------MENU--------\n"
+                               +"1. Insertar mascota\n"
+                               +"2. Actualizar mascota\n"
+                               +"3. Eliminar mascota\n"
+                               +"4. Buscar mascota por nombre\n"
+                               +"5. Listar todas las mascotas\n"
+                               +"6. Salir\n"
+                               +"Ingrese una opcion: ");
+            opc=sc.nextByte();
+            switch(opc){
+                case 1:
+                break;
+                case 2: 
+                break;
+                case 3: 
+                break;
+                case 4: 
+                break;
+                case 5: 
+                break;
+                
+                
+            }
+            
+        } while (opc!=6);
+
 
 
         
