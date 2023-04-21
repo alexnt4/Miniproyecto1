31f4033840cf4ca7f66c07739ff448ae557257ee
Alex García Castañeda <kend1123ue@gmail.com>
2023-04-20 20:42:09 -0500

    Agregando metodo insertar mascotas

--- src/main/App.java
+++ src/main/App.java
@@ -1,10 +1,14 @@
 package main;
+import java.util.ArrayList;
 import java.util.Scanner;
 import logica.Gato;
+import logica.Mascota;
 import logica.Perro;
 import logica.RazaPerro;
 
 public class App {
+
+    static ArrayList<Mascota> mascotas = new ArrayList<Mascota>();
 
     //Menu principal
     public static void MenuPrincipal() {
@@ -21,7 +25,7 @@ public class App {
                                +"Ingrese una opcion: ");
             opc=sc.nextByte();
             switch(opc){
-                case 1:
+                case 1: insertarMascota();
                 break;
                 case 2: 
                 break;
@@ -36,11 +40,96 @@ public class App {
         } while (opc!=6);  
         sc.close();
     }
+
+    
+    static Scanner scanner = new Scanner(System.in);
+
+    // funcion para insertar las mascotas
+    public static void insertarMascota() {
+        System.out.println("Seleccione el tipo de mascota a agregar:");
+        System.out.println("1. Perro");
+        System.out.println("2. Gato");
+        int opcion = scanner.nextInt();
+        scanner.nextLine(); // Limpiar el buffer del scanner
+        switch(opcion) {
+            case 1:
+                agregarPerro();
+                break;
+            case 2:
+                agregarGato();
+                break;
+            default:
+                System.out.println("Opción inválida");
+        }
+        
+    }
+
+    // funcion para agregar un perro, pido los atributos los atributos del gato y creo un objeto gato con los atributos
+    // TODO falta agregar el atributo RazaPerro
+    public static void agregarPerro(){
+        System.out.print("Ingrese el nombre del perro: ");
+        String nombrePerro = scanner.nextLine();
+        System.out.println("Ingrese la raza del perro");
+        //RazaPerro raza = scanner.();
+        System.out.print("Ingrese las vacunas del perro: ");
+        byte vacunasPerro = scanner.nextByte();
+        System.out.println("Ingrese el precio del perro: ");
+        double precioPerro = scanner.nextDouble();
+        System.out.println("Ingrese el pais de origen del perro: ");
+        String paisOrigenP = scanner.nextLine();
+
+        scanner.nextLine(); // Limpiar el buffer del scanner
+        //Perro nuevoPerro = new Perro(nombrePerro, raza, edadPerro, precioPerro, paisOrigen );
+        //mascotas.add(nuevoPerro);
+    }
+
+    // funcion para agregar un gato, pido los atributos los atributos del gato y creo un objeto gato con los atributos
+    public static void agregarGato(){
+        System.out.print("Ingrese el nombre del gato: ");
+        String nombreGato = scanner.nextLine();
+        System.out.print("Ingrese las vacunas del gato: ");
+        byte vacunasGato= scanner.nextByte();
+        System.out.println("Ingrese el precio del gato: ");
+        double precioGato = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Ingrese el pais de origen del gato: ");
+        String paisOrigenG = scanner.nextLine();
+      
+        Gato nuevoGato = new Gato(nombreGato, vacunasGato, precioGato, paisOrigenG );
+        mascotas.add(nuevoGato);
+
+        // lista las mascotas agregradas a modo de comprobar el funcionamiento del metodo insertarMascotas
+        for (int i = 0; i < mascotas.size(); i++) {
+            if (mascotas.get(i) instanceof Gato) {
+                Gato gato1 = (Gato) mascotas.get(i);
+                System.out.println(gato1.toString());
+                System.out.println("Nombre: " + gato1.getNombre());
+                System.out.println("Vacunas: " + gato1.getVacunas());
+                System.out.println("Precio : " + gato1.getPrecio());
+                System.out.println("Pais de origen : " + gato1.getPaisOrigen());
+                System.out.println("Mostrar id: " + gato1.getId());
+                gato1.mostrarInformacion();
+                
+
+            }
+        }
+    }
+
+
+
+
+
     public static void main(String[] args) throws Exception {
+
+        MenuPrincipal();
+        
+        
+
+
         
 
         // Prueba con objetos Perro
-        System.out.println("\nPruebas obj Perro");
+        /*System.out.println("\nPruebas obj Perro");
         Perro alfred = new Perro();
         alfred.mostrarInformacion();
 
@@ -78,7 +167,7 @@ public class App {
         System.out.println(pancho.saludar()); 
 
         //Metodo menu
-        App.MenuPrincipal(); 
+        App.MenuPrincipal(); */
 
     } 
 }
